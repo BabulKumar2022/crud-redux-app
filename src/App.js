c50c8671acd69e9410e922444c292a847d0e0eed
BabulKumar2022 <bb3583400@gmail.com>
2022-08-03 23:29:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import CreatePost from './components/CreatePost';
+import Posts from './components/Posts';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <Routes>
+          <Route path='/' element={<Posts></Posts>}></Route>
+          <Route path='createPost' element={<CreatePost></CreatePost>}></Route>
+      </Routes>
+     </div>
   );
 }
 
